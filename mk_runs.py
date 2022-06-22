8a570f94d9b376a92c64170050a73dd7ce08b974
Peter Teuben <teuben@gmail.com>
2022-06-22 10:34:34 -0400

    upd with obsnum.args

--- mk_runs.py
+++ mk_runs.py
@@ -74,65 +74,77 @@ pars2['J165845.46+360543.8'] = ""
 pars2['J211704.27-191850.4'] = ""
 
 
+
 # below here no need to change code
 # ========================================================================
 
-#        helper function for populating obsnum dependant argument
-def getargs(obsnum, flags=True):
+#        helper function for populating obsnum dependant argument -- deprecated
+def getargs3(obsnum):
     """ search for <obsnum>.args
-        and in lmtoy.flags
     """
-    args = ""    
-    if flags:
-        f = 'lmtoy.flags'
-        if os.path.exists(f):
-            lines = open(f).readlines()
-            for line in lines:
-                if line[0] == '#': continue
-                args = args + line.strip() + " "
-        
     f = "%d.args" % obsnum
     if os.path.exists(f):
         lines = open(f).readlines()
+        args = ""
         for line in lines:
             if line[0] == '#': continue
             args = args + line.strip() + " "
-    return args
+        return args
+    else:
+        return ""
 
-#        specific parameters per obsnum will be in files <obsnum>.args
+#        specific parameters per obsnum will be in files <obsnum>.args -- deprecated
 pars3 = {}
 for s in on.keys():
     for o1 in on[s]:
         o = abs(o1)
-        pars3[o] = getargs(o)
+        pars3[o] = getargs3(o)
 
+#        obsnum.args is alternative single file pars file to set individual parameters
+pars4 = {}
+if os.path.exists("obsnum.args"):
+    lines = open("obsnum.args").readlines()
+    for line in lines:
+        if line[0] == '#': continue
+        w = line.split()
+        pars4[int(w[0])] = w[1:]
+        print('PJT',w[0],w[1:])
+
+def getargs(obsnum):
+    """ search for <obsnum> in obsnum.args
+    """
+    args = ""
+    if obsnum in pars4.keys():
+        print("PJT2:",obsnum,pars4[obsnum])
+        for a in pars4[obsnum]:
+            args = args + " " + a
+    return args
 
 run1  = '%s.run1'  % project
 run1a = '%s.run1a' % project
+run1b = '%s.run1b' % project
 run2  = '%s.run2' % project
 run2a = '%s.run2a' % project
 
 fp1 = open(run1,  "w")
 fp2 = open(run1a, "w")
-fp3 = open(run2,  "w")
-fp4 = open(run2a, "w")
+fp3 = open(run1b, "w")
 
-#                           record the obsnum min/max
-o_min =  999999
-o_max = -999999
+fp4 = open(run2,  "w")
+fp5 = open(run2a, "w")
 
 #                           single obsnum
 n1 = 0
 for s in on.keys():
     for o1 in on[s]:
         o = abs(o1)
-        cmd1 = "SLpipeline.sh obsnum=%d _s=%s %s admit=0 restart=1 " % (o,s,pars1[s])
-        cmd2 = "SLpipeline.sh obsnum=%d _s=%s %s admit=0 %s" % (o,s,pars2[s], pars3[o])
+        cmd1 = "SLpipeline.sh obsnum=%d _s=%s %s admit=0 restart=1 %s %s" % (o,s,pars1[s], pars2[s], getargs(o))
+        cmd2 = "SLpipeline.sh obsnum=%d _s=%s %s admit=0 restart=1" % (o,s,pars1[s])
+        cmd3 = "SLpipeline.sh obsnum=%d _s=%s %s admit=0 %s" % (o,s,pars2[s], getargs(o))
         fp1.write("%s\n" % cmd1)
         fp2.write("%s\n" % cmd2)
+        fp3.write("%s\n" % cmd3)
         n1 = n1 + 1
-        if o1 < o_min:  o_min=o1
-        if o1 > o_max:  o_max=o1        
 
 #                           combination obsnums
 n2 = 0        
@@ -148,16 +160,16 @@ for s in on.keys():
         else:
             obsnums = obsnums + ",%d" % o
     print('%s[%d/%d] :' % (s,n3,len(on[s])), obsnums)
-    cmd3 = "SLpipeline.sh _s=%s admit=0 restart=1 obsnums=%s" % (s, obsnums)
-    cmd4 = "SLpipeline.sh _s=%s admit=1 srdp=1  obsnums=%s" % (s, obsnums)
-    fp3.write("%s\n" % cmd3)
+    cmd4 = "SLpipeline.sh _s=%s admit=0 restart=1 obsnums=%s" % (s, obsnums)
+    cmd5 = "SLpipeline.sh _s=%s admit=1 srdp=1  obsnums=%s" % (s, obsnums)
     fp4.write("%s\n" % cmd4)
+    fp5.write("%s\n" % cmd5)
     n2 = n2 + 1
 
 print("A proper re-run of %s should be in the following order:" % project)
-print(run1)
-print(run2)
 print(run1a)
+print(run2)
+print(run1b)
 print(run2a)
 print("Where there are %d single obsnum runs, and %d combination obsnums" % (n1,n2))
-print("Range of obsnums: %d - %d" % (o_min, o_max))
+
